d4ffbd874f93f524f484919dc5547bc33cd616db
kkito <kkitokkito@126.com>
2019-09-11 09:15:57 +0800

    log content update

--- src/utils/LogUtil.ts
+++ src/utils/LogUtil.ts
@@ -1,14 +1,18 @@
 export class LogUtil {
   public static debug(content: any) {
-    console.log(content);
+    this._log('debug', content)
   }
   public static info(content: any) {
-    console.log(content);
+    this._log('info', content)
   }
   public static error(content: any) {
-    console.log(content);
+    this._log('error', content)
   }
   public static fatal(content: any) {
-    console.log(content);
+    this._log('fatal', content)
+  }
+
+  public static _log(level:string, content:any):void {
+    console.log(`${new Date().toLocaleString} - ${level}: ${content}`)
   }
 }
