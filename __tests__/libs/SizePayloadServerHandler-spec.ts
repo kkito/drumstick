405a56f776f1dcf2ebb2fd85a17341080fe27ac6
kkito <kkitokkito@126.com>
2019-09-11 09:44:26 +0800

    send content buffer with multi times

--- __tests__/libs/SizePayloadServerHandler-spec.ts
+++ __tests__/libs/SizePayloadServerHandler-spec.ts
@@ -13,3 +13,21 @@ test("regular size scenario", async done => {
   })
   socket.emit("data" , sendData)
 });
+
+test("send data with many times", async done => {
+  const socket = new net.Socket();
+  const handler = new SizePayloadServerHandler(socket);
+  const content = Buffer.from("this is a test");
+  const sendData = BufferUtil.sizeWithBuffer(content);
+  const mockCB = jest.fn()
+  handler.setDataReceiveCallback(mockCB)
+  handler.setPayloadPrepareCallback((data:Buffer) => {
+    expect(data.toString()).toEqual("this is a test")
+    expect(mockCB.mock.calls.length).toBe(4)
+    done()
+  })
+  socket.emit("data" , sendData.slice(0,1))
+  socket.emit("data" , sendData.slice(1,3))
+  socket.emit("data" , sendData.slice(3,8))
+  socket.emit("data" , sendData.slice(8))
+});
