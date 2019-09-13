9230008b4132fa59f119e882c1aa1fbe50ea583c
kkito <kkitokkito@126.com>
2019-09-13 09:46:17 +0800

    lint stage support

--- __tests__/libs/SizePayloadServerHandler-spec.ts
+++ __tests__/libs/SizePayloadServerHandler-spec.ts
@@ -7,11 +7,11 @@ test("regular size scenario", async done => {
   const handler = new SizePayloadServerHandler(socket);
   const content = Buffer.from("this is a test");
   const sendData = BufferUtil.sizeWithBuffer(content);
-  handler.setPayloadPrepareCallback((data:Buffer) => {
-    expect(data.toString()).toEqual("this is a test")
-    done()
-  })
-  socket.emit("data" , sendData)
+  handler.setPayloadPrepareCallback((data: Buffer) => {
+    expect(data.toString()).toEqual("this is a test");
+    done();
+  });
+  socket.emit("data", sendData);
 });
 
 test("send data with many times", async done => {
@@ -19,15 +19,31 @@ test("send data with many times", async done => {
   const handler = new SizePayloadServerHandler(socket);
   const content = Buffer.from("this is a test");
   const sendData = BufferUtil.sizeWithBuffer(content);
-  const mockCB = jest.fn()
-  handler.setDataReceiveCallback(mockCB)
-  handler.setPayloadPrepareCallback((data:Buffer) => {
-    expect(data.toString()).toEqual("this is a test")
-    expect(mockCB.mock.calls.length).toBe(4)
-    done()
-  })
-  socket.emit("data" , sendData.slice(0,1))
-  socket.emit("data" , sendData.slice(1,3))
-  socket.emit("data" , sendData.slice(3,8))
-  socket.emit("data" , sendData.slice(8))
+  const mockCB = jest.fn();
+  handler.setDataReceiveCallback(mockCB);
+  handler.setPayloadPrepareCallback((data: Buffer) => {
+    expect(data.toString()).toEqual("this is a test");
+    expect(mockCB.mock.calls.length).toBe(4);
+    done();
+  });
+  socket.emit("data", sendData.slice(0, 1));
+  socket.emit("data", sendData.slice(1, 3));
+  socket.emit("data", sendData.slice(3, 8));
+  socket.emit("data", sendData.slice(8));
+});
+
+test("send multi requests", async done => {
+  const socket = new net.Socket();
+  const handler = new SizePayloadServerHandler(socket);
+  const content = Buffer.from("this is a test");
+  const sendData = BufferUtil.sizeWithBuffer(content);
+  const mockCB = jest.fn();
+  handler.setDataReceiveCallback(mockCB);
+  handler.setPayloadPrepareCallback((data: Buffer) => {
+    expect(data.toString()).toEqual("this is a test");
+    // expect(mockCB.mock.calls.length).toBe(4)
+    done();
+  });
+  socket.emit("data", sendData);
+  socket.emit("data", sendData);
 });
--- src/utils/LogUtil.ts
+++ src/utils/LogUtil.ts
@@ -1,18 +1,18 @@
 export class LogUtil {
   public static debug(content: any) {
-    this._log('debug', content)
+    this._log("debug", content);
   }
   public static info(content: any) {
-    this._log('info', content)
+    this._log("info", content);
   }
   public static error(content: any) {
-    this._log('error', content)
+    this._log("error", content);
   }
   public static fatal(content: any) {
-    this._log('fatal', content)
+    this._log("fatal", content);
   }
 
-  public static _log(level:string, content:any):void {
-    console.log(`${new Date().toLocaleString} - ${level}: ${content}`)
+  public static _log(level: string, content: any): void {
+    console.log(`${new Date().toLocaleString()} - ${level}: ${content}`);
   }
 }
