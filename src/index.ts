124c3c8c5d037afaebd79a966fc59d8cebee82a9
kkito <kkitokkito@126.com>
2019-09-21 14:36:45 +0800

    export classes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
 import { DrumstickClient } from "./libs/DrumstickClient";
-export { DrumstickClient };
+import { DrumstickResponse, IDSResponse } from "./libs/Response";
+import { BufferUtil } from "./utils/BufferUtil";
+import { LogUtil } from "./utils/LogUtil";
+import { RC4Util } from "./utils/RC4Util";
+export {
+  DrumstickClient,
+  DrumstickResponse,
+  IDSResponse,
+  BufferUtil,
+  LogUtil,
+  RC4Util
+};
