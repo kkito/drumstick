e2cafafec3f3f2bdabf2aff9147e9b3647f846e1
kkito <kkitokkito@126.com>
2019-09-21 14:51:38 +0800

    add method ensureRequest

--- src/libs/DrumstickClient.ts
+++ src/libs/DrumstickClient.ts
@@ -2,8 +2,38 @@ import * as iconv from "iconv-lite";
 import { RC4PayloadClient } from "./RC4PayloadClient";
 import { DrumstickResponse, IDSResponse } from "./Response";
 
+export interface IDrumstickRequestOptions {
+  retry?: number;
+}
+
 export class DrumstickClient extends RC4PayloadClient {
   protected onDataHandler?: (data: Buffer) => void;
+
+  public async ensureRequest(
+    url: string,
+    headers: any = {},
+    encoding = DrumstickResponse.ENCODING_BINARY,
+    requestOptions: IDrumstickRequestOptions
+  ): Promise<IDSResponse> {
+    const retryTimes = requestOptions.retry || 6;
+    for (let i = 0; i < retryTimes; i++) {
+      try {
+        const result = await this.request(url, headers, encoding);
+        return result;
+      } catch (err) {
+        // TODO err const
+        if (
+          err !== "timeout" ||
+          err !== "This socket has been ended by the other party"
+        ) {
+          this.close();
+          throw new Error(err);
+        }
+      }
+    }
+    this.close();
+    throw new Error("timeout for retry");
+  }
 
   public async request(
     url: string,
@@ -34,7 +64,11 @@ export class DrumstickClient extends RC4PayloadClient {
         } else {
           body = body.toString();
         }
-        resolve({ body, headers: result.headers, status: result.status });
+        resolve({
+          body,
+          headers: result.headers,
+          status: result.status
+        });
       };
       this.send(JSON.stringify(params));
     });
